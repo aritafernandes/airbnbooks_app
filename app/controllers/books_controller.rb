df988c8c92a7c6a046e7f4c1f044a1b7e78a4a43
gabriel dinis <gabriel.afonso989@gmail.com>
2023-05-29 18:16:01 +0100

    created a book model, created the seeds, created the routes index, show, new, create and created the controller with the actions index, show, new, create.

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,29 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user = current_user
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :details, :author, :year)
+  end
+end
