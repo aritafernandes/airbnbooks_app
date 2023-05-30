f050d3e70ec92069544f1ac4c9c461857318b9a9
rita fernandes <ritaestevesfernandes@gmail.com>
2023-05-30 11:43:44 +0100

    Create navigation links of the app

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "books#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :books, only: [:index, :new, :create, :show]
+  resources :books
 end
