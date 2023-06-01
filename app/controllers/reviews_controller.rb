8bf3e1f55240cf35945a56640995c33e61fff8d9
gabriel dinis <gabriel.afonso989@gmail.com>
2023-06-01 12:18:40 +0100

    create review & changed the bottom

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,23 @@
+class ReviewsController < ApplicationController
+  def new
+    @book = Book.find(params[:book_id])
+    @review = Review.new
+  end
+
+  def create
+    @book = Book.find(params[:book_id])
+    @review = Review.new(review_params)
+    @review.book = @book
+    if @review.save
+      redirect_to book_path(@book)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_30_161336) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_01_095951) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_161336) do
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.bigint "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -82,4 +90,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_161336) do
   add_foreign_key "bookings", "books"
   add_foreign_key "bookings", "users"
   add_foreign_key "books", "users"
+  add_foreign_key "reviews", "books"
 end
