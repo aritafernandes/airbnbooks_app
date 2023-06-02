1ceb485a7affbc07efd2997b2db9c31f659f335e
gabriel dinis <gabriel.afonso989@gmail.com>
2023-06-02 11:54:16 +0100

    change review-front_end and add rating to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_01_095951) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_02_081550) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_095951) do
     t.bigint "book_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating"
     t.index ["book_id"], name: "index_reviews_on_book_id"
   end
 
